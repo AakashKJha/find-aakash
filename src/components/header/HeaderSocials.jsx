9b5bb7d39bbeaee9514372a792d21ea3787e4ea0
aakash <aakashkumar2001jha@gmail.com>
2023-09-06 21:13:56 +0530

    changing cv

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -7,7 +7,7 @@ function HeaderSocials() {
   return (
     <div className='hader_socials'>
         <a href="https://www.linkedin.com/in/coderaakash/" target="_blank"><BsLinkedin /></a>
-        <a href="https://github.com/starkofficial" target="_blank"><BsGithub /></a>
+        <a href="https://github.com/AakashKJha" target="_blank"><BsGithub /></a>
         <a href="https://starkofficial.github.io/" target="_blank"><GiTechnoHeart /></a>
         </div>
   )
